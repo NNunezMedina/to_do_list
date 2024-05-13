ebed679673077111f9fb2ecea4ac58fe282b9395
NNunez <nathalynunezmedina@gmail.com>
2024-05-13 14:05:12 -0500

    Simple functioning to do list

---
+++ main.js
@@ -0,0 +1,35 @@
+const newItem = document.querySelector('#item_to_add')
+const ulElements = document.querySelector('#to_do_list')
+const addButton = document.querySelector('#addItem')
+const completedList = document.querySelector('#completed_list')
+
+
+function addItem(event) {
+    if(event.target.tagName !== 'INPUT') {
+        const newLiElement = document.createElement('li');
+        
+        const newCheckbox = document.createElement('input')
+        newCheckbox.type = 'checkbox';
+        
+        newLiElement.textContent = newItem.value;
+        newLiElement.appendChild(newCheckbox);
+        ulElements.appendChild(newLiElement);
+        newItem.value = '';
+
+        newCheckbox.addEventListener('change',itemCompleted);
+    }
+    }
+    addButton.addEventListener("click",addItem)
+
+    function itemCompleted(event) {
+        const checkbox = event.target;
+        const listItem = checkbox.parentNode;
+
+        if(checkbox.checked) {
+            completedList.appendChild(listItem);
+        }
+    }
+
+
+
+
